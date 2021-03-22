92b3731628b65ac8548bac753b43bdba60bfccef
latif vedic <latif@vedic.com>
2021-03-22 11:02:06 +0530

    1st maven commit

---
+++ src/main/java/com/myvedic/mavenproject1/Welcome.java
@@ -0,0 +1,8 @@
+package com.myvedic.mavenproject1;
+
+public class Welcome {
+    public static void main(String[] args){
+        //System.out.println("Hello, java with jenkins");
+	System.out.println("Welcome to Jenkins");
+}
+}
